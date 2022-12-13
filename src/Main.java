636419f991d8784461f82e8d4f30a6f410a11ff0
s3938278 <an6122003@gmail.com>
2022-12-13 17:04:13 +0700

    -- Initial commit for week 8 tutorial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input name: ");
+        scanner.nextLine();
+        System.out.println("Input address: ");
+        scanner.nextLine();
+        System.out.println("Input ages: ");
+        scanner.nextInt();
+
+        PrintWriter pw = null;
+
+        try{
+
+        }
+
+    }
+}
